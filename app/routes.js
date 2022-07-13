dc91155c8cc6b2dc5dffb613d2708ce62947c17d
Russ Poulter <russell.poulter@infinityworks.com>
2022-07-13 17:01:45 +0100

    some routing and added eng eligibiity

--- app/routes.js
+++ app/routes.js
@@ -3,21 +3,21 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
+// Run this code when a form is submitted to 'country'
 router.post('/country-answer', function (req, res) {
-
-    // Make a variable and give it the value from 'how-many-balls'
+    // Make a variable and give it the value from 'where-do-you-live'
     var country = req.session.data['where-do-you-live']
-
     // Check whether the variable matches a condition
     if (country == "England"){
-        res.redirect('/somthing')
+        res.redirect('order-lateral-flow-kits/condition')
     } else if (country == "Scotland") {
         res.redirect('/ineligible')
     } else if (country == "Northern Ireland"){
         res.redirect('/somthing-else')
-    } else if (country == "Wales"){
+    } else {
         res.redirect('/somthing-else-else')
     }
 })
 module.exports = router
+
+
